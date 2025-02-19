969e451d4cbbffa0cf54c36cd930e08bc5d08619
sayalaruano <sayalaruano@users.noreply.github.com>
2025-02-19 22:43:43 +0000

    deploy: 2f955ccbdef19daec7c60efb8ce7f5a8bb8af60e

--- streamlit_report/sections/Network_Analysis/Phyla_Association_Networks.py
+++ streamlit_report/sections/Network_Analysis/Phyla_Association_Networks.py
@@ -1,8 +1,8 @@
-import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
-import streamlit as st
+import pandas as pd
 df_index = 1
 import requests
+import streamlit as st
 
 st.markdown('''<h3 style='text-align: center; color: #023558;'>Phyla Association Networks</h3>''', unsafe_allow_html=True)
 st.markdown('''<h4 style='text-align: center; color: #2b8cbe;'>Phyla Counts Subset</h4>''', unsafe_allow_html=True)
