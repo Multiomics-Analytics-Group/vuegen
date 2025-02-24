486203e93bbec2adaa94a45111ebb7eef65a52a3
enryH <enryH@users.noreply.github.com>
2025-02-24 14:04:14 +0000

    deploy: b6a09cc658053ee9a2c67bd8922b620d817e1a34

--- streamlit_report/sections/Exploratory_Data_Analysis/Sample_Exploration.py
+++ streamlit_report/sections/Exploratory_Data_Analysis/Sample_Exploration.py
@@ -1,10 +1,10 @@
-import altair as alt
-import streamlit as st
 import requests
-df_index = 1
-import json
-from st_aggrid import AgGrid, GridOptionsBuilder
+import altair as alt
 import pandas as pd
+import streamlit as st
+import json
+df_index = 1
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 st.markdown('''<h3 style='text-align: center; color: #023558;'>Sample Exploration</h3>''', unsafe_allow_html=True)
 st.markdown('''<h4 style='text-align: center; color: #2b8cbe;'>Metadata Random Subset</h4>''', unsafe_allow_html=True)
