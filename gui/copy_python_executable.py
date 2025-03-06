068239ad9fdcde7c5915eec4f925b2832780e578
Henry Webel <heweb@dtu.dk>
2025-03-06 07:47:47 +0100

    :construction: move python file to main folder

--- gui/copy_python_executable.py
+++ gui/copy_python_executable.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 python_exe = Path(sys.executable)  # .with_stem("python")
 print("Copying python executable:", python_exe)
-shutil.copy(python_exe, "dist/vuegen_gui/_internal/python")
+shutil.copy(python_exe, "dist/vuegen_gui/python")
 print("Done.")
