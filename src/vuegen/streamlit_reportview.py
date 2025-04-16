744a5387b4b4611f902ff836cfe2856bdd1612cb
sayalaruano <sebasar1245@gmail.com>
2025-04-16 12:38:55 +0200

    🩹 Add balck format

--- src/vuegen/streamlit_reportview.py
+++ src/vuegen/streamlit_reportview.py
@@ -758,7 +758,7 @@ with open('{(Path("..") / markdown.file_path).as_posix()}', 'r') as markdown_fil
 
     def _generate_html_content(self, html) -> List[str]:
         """
-        Generate content for an HTML component. 
+        Generate content for an HTML component.
 
         Parameters
         ----------
@@ -818,7 +818,7 @@ with open('{(Path("..") / html.file_path).as_posix()}', 'r', encoding='utf-8') a
 
     def _generate_apicall_content(self, apicall) -> List[str]:
         """
-        Generate content for an API component. This method handles the API call and formats 
+        Generate content for an API component. This method handles the API call and formats
         the response for display in the Streamlit app.
 
         Parameters
@@ -865,8 +865,8 @@ with open('{(Path("..") / html.file_path).as_posix()}', 'r', encoding='utf-8') a
         """
         Generate content to render a ChatBot component, supporting standard and Ollama-style streaming APIs.
 
-        This method builds and returns a list of strings, which are later executed to create the chatbot 
-        interface in a Streamlit app. It includes user input handling, API interaction logic, response parsing, 
+        This method builds and returns a list of strings, which are later executed to create the chatbot
+        interface in a Streamlit app. It includes user input handling, API interaction logic, response parsing,
         and conditional rendering of text, source links, and HTML subgraphs.
 
         The function distinguishes between two chatbot modes:
