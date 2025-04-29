03d5700533a9f69ece114a9b51341592cb76e9c8
sayalaruano <sebasar1245@gmail.com>
2025-04-29 12:09:47 +0200

    ✨ Add emoji to the turn off button and make it the size of sidebar

--- src/vuegen/streamlit_reportview.py
+++ src/vuegen/streamlit_reportview.py
@@ -164,7 +164,7 @@ class StreamlitReportView(r.WebAppReportView):
                     report_nav = st.navigation(sections_pages)
                     
                     # Following https://discuss.streamlit.io/t/close-streamlit-app-with-button-click/35132/5
-                    exit_app = st.sidebar.button("Shut Down App")
+                    exit_app = st.sidebar.button("Shut Down App", icon=":material/power_off:", use_container_width=True)
                     if exit_app:
                         st.toast("Shutting down the app...")
                         time.sleep(1)
