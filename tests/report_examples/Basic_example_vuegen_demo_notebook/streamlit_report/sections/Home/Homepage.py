ddf195f54d693876c9bb2aba80fff7759812f1d9
Henry Webel <heweb@dtu.dk>
2025-06-20 10:25:13 +0200

    :sparkles: add current main state of streamlit report Python files

---
+++ tests/report_examples/Basic_example_vuegen_demo_notebook/streamlit_report/sections/Home/Homepage.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import streamlit as st
+import requests
+st.markdown('''<p style='text-align: center; color: #000000;'>A general description of the report.
+</p>''', unsafe_allow_html=True)
+st.markdown('''<h4 style='text-align: center; color: #2b8cbe;'>Description</h4>''', unsafe_allow_html=True)
+
+with open('docs/example_data/Basic_example_vuegen_demo_notebook/description.md', 'r') as markdown_file:
+    markdown_content = markdown_file.read()
+
+st.markdown(markdown_content, unsafe_allow_html=True)
+
+footer = '''<style type="text/css">
+.footer {
+    position: relative;
+    left: 0;
+    width: 100%;
+    text-align: center;
+}
+</style>
+<footer class="footer">
+    This report was generated with 
+    <a href="https://github.com/Multiomics-Analytics-Group/vuegen" target="_blank">
+        <img src="https://raw.githubusercontent.com/Multiomics-Analytics-Group/vuegen/main/docs/images/vuegen_logo.svg" alt="VueGen" width="65px">
+    </a>
+    | Copyright 2025 <a href="https://github.com/Multiomics-Analytics-Group" target="_blank">
+        Multiomics Network Analytics Group (MoNA)
+    </a>
+</footer>'''
+
+st.markdown(footer, unsafe_allow_html=True)
