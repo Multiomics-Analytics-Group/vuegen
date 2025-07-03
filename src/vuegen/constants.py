60f3ecf6f64393c032059d711648cc94f649fc17
Sebastián Ayala Ruano <52267585+sayalaruano@users.noreply.github.com>
2025-07-03 15:42:01 +0200

    📝 Update logo and citation (#145)
    * 📝 Update logo and citation
    
    * ✅ update logo link in test
    
    - will be only present on main branch (or HEAD) after merging of PR is completed
    
    * :bug: add update of logo to chatbot report example file
    
    ---------
    
    Co-authored-by: Henry Webel <heweb@dtu.dk>

--- src/vuegen/constants.py
+++ src/vuegen/constants.py
@@ -6,6 +6,6 @@ GITHUB_ORG_URL_BRACKETS = "{https://github.com/Multiomics-Analytics-Group}"
 REPO_URL = "https://github.com/Multiomics-Analytics-Group/vuegen"
 LOGO_URL = (
     "https://raw.githubusercontent.com/Multiomics-Analytics-Group/"
-    "vuegen/main/docs/images/vuegen_logo.svg"
+    "vuegen/HEAD/docs/images/logo/vuegen_logo.svg"
 )
 TIMEOUT: int = 60
